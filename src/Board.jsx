ab72f3f7d3ec0f514d830aac048b00cadb1ecb91
Shamiul40 <shamiul4040@gmail.com>
2025-11-05 09:11:10 +0600

    first commit

---
+++ src/Board.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+
+function Board() {
+  
+
+  return (
+    <>
+   
+       <div>
+          <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">x</button>
+          <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">x</button>
+          <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">x</button>
+       </div>
+        <div>
+          <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">x</button>
+          <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">x</button>
+          <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">x</button>
+       </div>
+        <div>
+          <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">x</button>
+          <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">x</button>
+          <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">x</button>
+       </div>
+    </>
+  )
+}
+
+export default Board
