5494f1bb3f9c4b61a31e4ad72d32d1619d096776
Shamiul40 <shamiul4040@gmail.com>
2025-11-05 09:20:49 +0600

    props send to square

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,5 +1,13 @@
 import './App.css'
 
+
+function Square({value}) {
+  return (
+    <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">{value}</button>
+  )
+}
+
+
 function Board() {
   
 
@@ -7,20 +15,21 @@ function Board() {
     <>
    
        <div>
-          <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">x</button>
-          <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">x</button>
-          <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">x</button>
+          <Square value="1" />
+          <Square value="2" />
+          <Square value="3" />
        </div>
         <div>
-          <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">x</button>
-          <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">x</button>
-          <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">x</button>
+          <Square value="4" />
+          <Square value="5" />
+          <Square value="6" />
        </div>
         <div>
-          <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">x</button>
-          <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">x</button>
-          <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">x</button>
+          <Square value="7" />
+          <Square value="8" />
+          <Square value="9" />
        </div>
+        
     </>
   )
 }
