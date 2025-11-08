8ed39505e53ddb4d90af8a0b3193f5312f3dec8e
Shamiul40 <shamiul4040@gmail.com>
2025-11-08 10:01:33 +0600

    state change and pass data x to all square when clicked

--- src/Board.jsx
+++ src/Board.jsx
@@ -21,26 +21,29 @@ function Board() {
 
     const [square, setSquare]= useState([Array(9).fill(null)]); 
     
-    const handleClick =()=>{
-      console.log("clicked")
+    function handleClick (i) {
+     const nextSquare = square.slice();
+     nextSquare[i]= "X";
+     setSquare(nextSquare);
+     console.log("clicked")
     }
 
   return (
     <>
       <div className="flex">
-        <Square value={square[1]} onSquareChange={handleClick} />
-        <Square value={square[2]} />
-        <Square value={square[3]} />
+        <Square value={square[0]} onSquareChange={()=>handleClick(0)} />
+        <Square value={square[1]} onSquareChange={()=>handleClick(1)}/>
+        <Square value={square[2]}  onSquareChange={()=>handleClick(2)}/>
       </div>
       <div className="flex">
-        <Square value={square[4]}/>
-        <Square value={square[5]}/>
-        <Square value={square[6]}/>
+        <Square value={square[3]} onSquareChange={()=>handleClick(3)}/>
+        <Square value={square[4]} onSquareChange={()=>handleClick(4)}/>
+        <Square value={square[5]} onSquareChange={()=>handleClick(5)}/>
       </div>
       <div className="flex">
-        <Square value={square[7]}/>
-        <Square value={square[8]}/>
-        <Square value={square[9]}/>
+        <Square value={square[6]} onSquareChange={()=>handleClick(6)}/>
+        <Square value={square[7]} onSquareChange={()=>handleClick(7)}/>
+        <Square value={square[8]} onSquareChange={()=>handleClick(8)}/>
       </div>
     </>
   );
